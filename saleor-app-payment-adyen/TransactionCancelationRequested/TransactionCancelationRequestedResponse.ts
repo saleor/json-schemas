cc41da5f2d0392a61c02b60127825955882f347e
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-10-10 16:32:14 +0200

    Rename files

--- saleor-app-payment-adyen/TransactionCancelationRequested/TransactionCancelationRequestedResponse.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Replace with zod type
-export interface TransactionCancelationRequestedResponse {
-  pspReference?: string;
-  result?: "CANCEL_SUCCESS" | "CANCEL_FAILURE";
-  amount?: number;
-  time?: string;
-  externalUrl?: string;
-  message?: string;
-}
--- saleor-app-payment-adyen/TransactionRefundRequesed/TransactionRefundRequestedResponse.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Replace with zod type
-export interface TransactionRefundRequestedResponse {
-  pspReference?: string;
-  result?: "REFUND_SUCCESS" | "REFUND_FAILURE";
-  amount?: number;
-  time?: string;
-  externalUrl?: string;
-  message?: string;
-}
